3424bb411d238c70fa05370999ee9b3cdf8cf0ef
Tomokiyo-H <2301179@s.asojuku.ac.jp>
2025-10-20 12:09:29 +0900

    仮・最終画面

--- final_result.php
+++ final_result.php
@@ -1,1 +1,27 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<title>最終画面</title>
+
+<div class="container">
+  <h1>おわりー！</h1>
+  <p class="correct">せいかいのかずは....</p>
+
+  <?php
+    // 正解数と総問題数をGETパラメータから取得
+    $total_questions = $_GET['total'] ?? 10;
+    $correct = $_GET['correct'] ?? 0;
+  ?>
+
+  <div class="score-box">
+    <?php echo $total_questions . "のうち " . $correct . "もん！"; ?>
+  </div>
+  <br>// homeに戻るボタン
+    <a href="Home.php" class="button">よくできました！</a>
+</div>
+
+</body>
+</html>
+
 
